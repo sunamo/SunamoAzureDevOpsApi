fa0acb6b62d4b13dba6441cbd78e30c26c0d86b8
Radek Jancik <radek.jancik@usys.cz>
2026-04-06 19:35:37 +0200

    26.4.6.6 feat: Improved code quality

--- RunnerAzureDevOpsApi/Program.cs
+++ RunnerAzureDevOpsApi/Program.cs
@@ -1,5 +1,2 @@
 // variables names: ok
-// EN: Variable names have been checked and replaced with self-descriptive names
-// CZ: Názvy proměnných byly zkontrolovány a nahrazeny samopopisnými názvy
-// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
